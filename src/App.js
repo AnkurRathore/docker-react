eac7955587338a402dd0355d56cc52594623a537
Ankur Rathore <rathore.ankur@gmail.com>
2019-07-20 16:06:43 +0530

    testing travis config

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Change is Working!!!Great!!        </p>
+         Automatic Deployment is Working!! </p>
         <a
           className="App-link"
           href="https://reactjs.org"
